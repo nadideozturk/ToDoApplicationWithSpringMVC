22a8df76b74c2a1cc0613e8cfd82b6a17af93cb7
Nadide Ozturk <nadidebaca@gmail.com>
2018-02-27 14:13:53 -0800

    ViewResolver is added to todo-servlet.xml

--- src/main/java/com/springmvc/LoginController.java
+++ src/main/java/com/springmvc/LoginController.java
@@ -8,8 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LoginController {
 	
 	@RequestMapping(value="/login")
-	@ResponseBody
 	public String sayHello() {
-		return "Hello world";
+		return "login";
 	}
 }
