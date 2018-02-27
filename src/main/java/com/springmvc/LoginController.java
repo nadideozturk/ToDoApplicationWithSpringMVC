52c7c74f20494a121031812d122a7192bfc8eebf
Nadide Ozturk <nadidebaca@gmail.com>
2018-02-27 14:55:51 -0800

    Login post request's controller added and redirect to welcome page

--- src/main/java/com/springmvc/LoginController.java
+++ src/main/java/com/springmvc/LoginController.java
@@ -1,14 +1,23 @@
 package com.springmvc;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+
+
 
 @Controller
 public class LoginController {
 	
-	@RequestMapping(value="/login")
-	public String sayHello() {
+	@RequestMapping(value="/login", method = RequestMethod.GET)
+	public String showLoginPage() {
 		return "login";
+	}
+	
+	@RequestMapping(value="/login", method = RequestMethod.POST)
+	public String handleLoginRequest(@RequestParam String userName,@RequestParam String password, ModelMap model) {
+		model.put("userName", userName);
+		model.put("password", password);
+		return "welcome";
 	}
 }
